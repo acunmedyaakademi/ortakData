3121ee18dd5e6c827d2f94bbf92360a49c125fd3
Mert Öztürk <mert32139@gmail.com>
2024-05-04 14:55:33 +0300

    fonksiyon guncellendi ve istenilen limit getirilmesi etkinleştirildi

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,28 @@
-let limit=0
-const BASE_URL = `https://dummyjson.com/recipes?limit=${limit}`
 const datarecipiesCont = document.querySelector(".data-recipes")
-async function fetchProduct(){
-    const recipes = await fetch(`${BASE_URL}`)
+const exploregrid = document.querySelector(".explore-grids")
+
+async function fetchProduct(limit){
+    const BASE_URL = `https://dummyjson.com/recipes?limit=${limit}`
+    const recipes = await fetch(BASE_URL)
     const json = await recipes.json()
     return json
 }
 
-async function recipesProduct(){
-    const recipiesSelect = await fetchProduct()
+async function recipesProduct(limit){
+    const recipiesSelect = await fetchProduct(limit)
     const product = recipiesSelect.recipes
 
     return product
 }
 
 async function productRecipes(){
-    console.log(await recipesProduct());
+    const exploreRecipies = await recipesProduct(6)
+    console.log(exploreRecipies);
+    for(const exploreProduct of exploreRecipies){
+        exploregrid.innerHTML += `
+            <div class="">
+        `
+    }
 }
 
-logor();
+productRecipes();
